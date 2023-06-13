25178ec015b177e44250d1a5925fc71ccdc92f2b
Szymon Rąpała <58051054+SzymiDjud@users.noreply.github.com>
2023-06-13 20:44:50 +0200

    Update Register.jsx

--- kcal-app/pages/Register.jsx
+++ kcal-app/pages/Register.jsx
@@ -38,7 +38,7 @@ export default function Register(props){
             fetch(process.env.API_URL + `api/auth/register/`,{
                 method: 'POST',
                 headers: {
-                    
+                    'Content-Type' : 'application/json',
                     
                 },
                 body: JSON.stringify(values),
